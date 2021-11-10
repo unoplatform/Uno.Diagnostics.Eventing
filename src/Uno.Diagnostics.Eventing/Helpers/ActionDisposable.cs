22aaf02e974c1d8c33eafad0c9c279e1f9b2a3e9
Jerome Laban <jerome.laban@nventive.com>
2021-11-09 20:50:40 -0500

    fix: Remove Uno.Core dependency completely

---
+++ src/Uno.Diagnostics.Eventing/Helpers/ActionDisposable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uno.Diagnostics.Eventing.Helpers
+{
+	internal class ActionDisposable : IDisposable
+	{
+		private readonly Action _action;
+
+		public ActionDisposable(Action action)
+		{
+			_action = action;
+		}
+
+		public void Dispose() => _action?.Invoke();
+	}
+}
